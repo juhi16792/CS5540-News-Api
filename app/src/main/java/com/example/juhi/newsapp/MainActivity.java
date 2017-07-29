9ed0aa0435758fd9bde6ab9a9c6c3dc634653c7e
juhi16792 <lrjuheeshwari16@gmail.com>
2017-07-28 22:12:58 -0700

    task 6 and 7 done

--- app/src/main/java/com/example/juhi/newsapp/MainActivity.java
+++ app/src/main/java/com/example/juhi/newsapp/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.juhi.newsapp;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.AsyncTaskLoader;
 import android.support.v4.content.Loader;
@@ -18,7 +20,11 @@ import android.view.View;
 import android.widget.ProgressBar;
 
 import com.example.juhi.newsapp.data.Contract;
+import com.example.juhi.newsapp.data.DBHelper;
+import com.example.juhi.newsapp.data.DBUtils;
 import com.example.juhi.newsapp.utilities.NewsAdapter;
+import com.example.juhi.newsapp.utilities.NewsJob;
+import com.example.juhi.newsapp.utilities.ScheduleUtils;
 
 
 // Adding loader manager and callbacks
@@ -47,9 +53,30 @@ public abstract class MainActivity extends AppCompatActivity implements LoaderMa
         rv.setLayoutManager(layoutManager);
         rv.setHasFixedSize(true);
         mProgressIndicator = (ProgressBar) findViewById(R.id.progressBar1);
-        
+
+        //Task 6 -> check if the app is installed or not. If it is not loaded, load data into db
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean isNotInstalled = prefs.getBoolean("isNotInstalled",true);
+
+        if(isNotInstalled){
+            LoaderManager loaderManager = getSupportLoaderManager();
+            loaderManager.restartLoader(LOADER, null, this).forceLoad();
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putBoolean("isfirst", false);
+            editor.commit();
+        }
+        ScheduleUtils.scheduleRefresh(this);
     }
 
+    @Override
+    protected void onStart(){
+        super.onStart();
+        // Task 4. Get a writable database reference and store in db
+        db = new DBHelper(MainActivity.this).getReadableDatabase();
+        cur = DBUtils.getAll(db);
+        newsAdapter = new NewsAdapter(cur, this);
+        rv.setAdapter(newsAdapter);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -67,7 +94,7 @@ public abstract class MainActivity extends AppCompatActivity implements LoaderMa
             return true;
         }
 
-        return true;
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -82,7 +109,7 @@ public abstract class MainActivity extends AppCompatActivity implements LoaderMa
             @Override
             public Void loadInBackground() {
                 // HW4: 7. Refresh articles method
-           //     NewsJob.refreshArticles(MainActivity.this);
+                NewsJob.refreshArticles(MainActivity.this);
                 return null;
             }
         };
@@ -94,6 +121,14 @@ public abstract class MainActivity extends AppCompatActivity implements LoaderMa
     @Override
     public void onLoadFinished(Loader<Void> loader, Void data){
         mProgressIndicator.setVisibility(View.INVISIBLE);
+        // Task 7 :- get info from db
+        db = new DBHelper(MainActivity.this).getReadableDatabase();
+        cur = DBUtils.getAll(db);
+
+        // Task 7 :- Reset data in recyclerview
+        newsAdapter = new NewsAdapter(cur, this);
+        rv.setAdapter(newsAdapter);
+        newsAdapter.notifyDataSetChanged();
     }
 
     // Task 4 -- update onListItemClick to include Cursor --> Completed
